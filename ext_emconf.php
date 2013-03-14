8516006fb9921c2a35e18f9c180b7d2a9ec1cdc7
Julian Lochstampfer <lochstampfer@b13.de>
2013-03-14 12:41:01 +0100

    Initial Upload

---
+++ ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "amazingfancybox".
+ *
+ * Auto generated 14-03-2013 12:48
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'FancyBox 2 - The Amazing Way',
+	'description' => 'This extension includes FancyBox2 in a way that works with Image Content Elements and custom extensions etc. Works with t3jquery. If you use this on a commercial website, make sure to check the FancyBox license: http://www.fancyapps.com/fancybox/#license.',
+	'category' => 'fe',
+	'author' => 'Benjamin Mack',
+	'author_email' => 'typo3@b13.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'b:dreizehn, Stuttgart',
+	'version' => '1.0.0',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+			't3jquery' => '',
+		),
+	),
+	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"cd40";s:17:"ext_localconf.php";s:4:"fa43";s:14:"ext_tables.php";s:4:"3606";s:14:"ext_tables.sql";s:4:"d41c";s:18:"importprojects.php";s:4:"767c";s:37:"Classes/Controller/MainController.php";s:4:"8124";s:33:"Classes/Domain/Model/Category.php";s:4:"d687";s:32:"Classes/Domain/Model/Project.php";s:4:"c0f5";s:47:"Classes/Domain/Repository/ProjectRepository.php";s:4:"0baa";s:49:"Classes/ViewHelpers/Widget/PaginateViewHelper.php";s:4:"9be4";s:60:"Classes/ViewHelpers/Widget/Controller/PaginateController.php";s:4:"d3bc";s:21:"Configuration/Tca.php";s:4:"4aaf";s:32:"Configuration/FlexForms/list.xml";s:4:"2292";s:38:"Configuration/TypoScript/constants.txt";s:4:"698a";s:34:"Configuration/TypoScript/setup.txt";s:4:"4a71";s:33:"Resources/Private/Language/db.xml";s:4:"8e38";s:40:"Resources/Private/Language/locallang.xml";s:4:"ca19";s:42:"Resources/Private/Templates/Main/List.html";s:4:"a13e";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"6ac9";s:35:"Resources/Public/Icons/category.gif";s:4:"cd40";s:34:"Resources/Public/Icons/project.gif";s:4:"cd40";s:37:"Resources/Public/JavaScript/carpus.js";s:4:"a7e7";s:37:"Resources/Public/JavaScript/jquery.js";s:4:"e59f";}',
+	'suggests' => 
+	array (
+	),
+);
+
+?>
